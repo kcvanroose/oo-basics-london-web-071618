8ba8b21d2cc8a293e3238219c02b44e2f5e35253
casey van-roose <kcvanroose@gmail.com>
2018-07-18 12:38:27 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,16 +14,6 @@ class Shoe
   def color
     @color
   end
-
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
  
-  def genre
-    @genre
-  end
+  
 end
