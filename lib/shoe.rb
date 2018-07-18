c672e3f7a401316a1bf84adc1291f1548f2a62aa
casey van-roose <kcvanroose@gmail.com>
2018-07-18 12:37:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,41 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    @color
+  end
+  
+  def color
+    @color
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+   def author
+    @author
+  end
+  
+   def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+end
