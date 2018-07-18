7dc04409567a849ac133eac8918da12aad0545fd
casey van-roose <kcvanroose@gmail.com>
2018-07-18 12:38:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,19 +14,7 @@ class Shoe
   def color
     @color
   end
-  
-  def author=(author)
-    @author = author
-  end
-  
-   def author
-    @author
-  end
-  
-   def page_count=(num)
-    @page_count = num
-  end
- 
+
   def page_count
     @page_count
   end
